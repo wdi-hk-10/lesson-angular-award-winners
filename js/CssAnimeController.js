3c83ea90f5645181b511eb63b32990361880c067
Willie Tong <tongsinyin@gmail.com>
2016-03-17 15:47:14 +0800

    start-code

--- js/CssAnimeController.js
+++ js/CssAnimeController.js
@@ -2,8 +2,6 @@ var cssAnimeApp = angular.module('CssAnimeApp');
 
 cssAnimeApp.controller('CssAnimeController', ['$scope', function($scope){
   
-  // model data
-  $scope.newWinner = {name: ''};
   $scope.winners = [
     {name: 'Colin Firth'},
     {name: 'Jean Dujardin'},
@@ -12,42 +10,5 @@ cssAnimeApp.controller('CssAnimeController', ['$scope', function($scope){
     {name: 'Eddie Redmayne'},
     {name: 'Leonardo DiCaprio'}
   ];
-
-  // ng-class values
-  $scope.styles = {
-    'bold': false,
-    'italics': false
-  };
-  $scope.validation = {
-    'error': false,
-    'success': false
-  };
-  $scope.winnerButton = 'btn-default';
-
-  $scope.validateForm = function () {
-    if ($scope.newWinner.name.length === 0) {
-      $scope.validation.error = false;
-      $scope.validation.success = false;
-      $scope.winnerButton = 'btn-default';
-    }
-    else if ($scope.newWinner.name.length < 4){
-      $scope.validation.error = true;
-      $scope.validation.success = false;
-      $scope.winnerButton = 'btn-default';
-    } else {
-      $scope.validation.error = false;
-      $scope.validation.success = true;
-      $scope.winnerButton = 'btn-success';
-    }
-  };
-
-  $scope.changeStyle = function(style) {
-    $scope.styles[style] = !$scope.styles[style]
-  };
-
-  $scope.addWinner = function() {
-    $scope.winners.push({ name: $scope.newWinner.name });
-    $scope.newWinner.name = '';
-    $scope.validateForm();
-  };
+  
 }]);
